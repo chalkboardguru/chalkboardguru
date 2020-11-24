ecdc5470f2a62d40e959d4338b7b6718ad85782b
morgansgarage <scott.morgan7@gmail.com>
2020-11-24 15:59:34 -0600

    First Commit with working layout

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Box } from '@material-ui/core';
+
+class About extends Component {
+    render() {
+        return (
+            <Box>
+                About
+            </Box>
+        );
+    }
+}
+export default About
---
+++ src/Pages/Users.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Box } from '@material-ui/core';
+class Users extends Component {
+    render() {
+        return (
+            <Box>
+                Users
+            </Box>
+        );
+    }
+}
+export default Users
